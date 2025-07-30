33523e570434620df58727349068ac015edf382b
RuslanSemenchik <semenchik.r@gmail.com>
2025-07-30 17:22:41 +0200

    add Homework_02

---
+++ src/homeworks/Homework_02/data.js
@@ -0,0 +1,6 @@
+export const user = {
+  avatarURL: "https://upload.wikimedia.org/wikipedia/ru/b/bd/Homer_Simpson.png",
+    firstName_lastName: "Homer Simpson",
+    profession : "inspector",
+    hobby: "fast food",
+  };
